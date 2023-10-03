ed47d8f92eaaa40fe76f2ce71208fba50e1a6fbe
JC <cjoaquinangel@gmail.com>
2023-10-03 15:03:37 -0600

    angular and routing generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FacturasComponent } from './components/facturas/facturas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Routes in object path and component
+  {path:'',component:DashboardComponent},
+  {path:'index',component:DashboardComponent},
+  {path:'facturas',component:FacturasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents:Array<any>=[DashboardComponent,FacturasComponent];
+export const routingWithProviders:Array<any>=[];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+
   ],
   imports: [
     BrowserModule,
@@ -16,3 +19,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
